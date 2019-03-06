f60a0f7a0ace11812de99de58278e24926e531a5
gaogep <zpf1893@qq.com>
2019-03-06 22:27:05 +0800

    添加了最基础的登录登出功能

--- app/blueprints/auth.py
+++ app/blueprints/auth.py
@@ -1,3 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, flash, render_template, redirect, url_for
+from flask_login import login_user, logout_user, login_required
+
+from ..forms import LoginForm
+from ..models import User
+from ..tools import redirect_back
+
 
 auth_bp = Blueprint('auth', __name__)
+
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        remember = form.remember.data
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            login_user(user, remember)
+            flash('登录成功', 'success')
+        else:
+            flash('登录失败', 'warning')
+    return render_template('login.html')
+
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('你已登出', 'success')
+    return redirect_back()
--- app/extensions.py
+++ app/extensions.py
@@ -5,6 +5,8 @@ from flask_wtf import CSRFProtect
 from flask_login import LoginManager
 from flask_moment import Moment
 
+from .models import User
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -12,3 +14,12 @@ migrate = Migrate()
 csrf = CSRFProtect()
 login_manager = LoginManager()
 moment = Moment()
+
+
+@login_manager.user_loader
+def get_user(user_id):
+    return User.query.get(int(user_id))
+
+
+login_manager.login_view = 'auth.login'
+login_manager.login_message_category = 'warning'
