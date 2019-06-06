98b731f0219e1a72ef5d20cdeff8aba99bf92296
gaogep <zpf1893@qq.com>
2019-06-06 22:41:13 +0800

    部署前的准备

--- app/extensions.py
+++ app/extensions.py
@@ -1,21 +1,17 @@
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_wtf import CSRFProtect
 from flask_login import LoginManager
 from flask_moment import Moment
-from flask_pagedown import PageDown
 from flask_whooshee import Whooshee
 from flask_debugtoolbar import DebugToolbarExtension
 
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-migrate = Migrate()
 csrf = CSRFProtect()
 login_manager = LoginManager()
 moment = Moment()
-pagedown = PageDown()
 whooshee = Whooshee()
 toolbar = DebugToolbarExtension()
 
